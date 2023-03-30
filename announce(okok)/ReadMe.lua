a499eb14d42c00f1b3c1a9f5883e7ec40f24352d
Adxyyu <129365722+Adxyyu@users.noreply.github.com>
2023-03-30 01:48:26 -0500

    Add files via upload

---
+++ announce(okok)/ReadMe.lua
@@ -0,0 +1,20 @@
+
+----Where To Find The OkOkNotify----
+
+To Use This Script You Need OkOkNotify Which Can Be Found Here
+https://okok.tebex.io/package/4724993
+
+There Discord Can Also Be Found Here
+https://discord.com/invite/okok
+
+----Ace Permissions----
+
+If you would like only a certain role in your discord to use this paste the following command in your SERVER.CFG
+
+EX:
+add_ace group.(YourGroupName) command.announce allow/deny
+
+
+If you need assistance pls contact me on discord
+
+(NickIsDad#1225)
---
+++ announce(okok)/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "adamant"
+game "gta5"
+author "NickIsDad#1225"
+description "Staff Announcment Script!"
+verison "v1.0.0"
+lua54 "yes"
+
+server_scripts {
+    "server.lua"
+}
+
+dependancies {
+    "mythic_notify"
